baeebb22c2ea4037bebd9b649ae44c524286bd99
IsMeduza <maxsanchezmasso@gmail.com>
2023-05-06 20:34:36 +0200

    V

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactForm = document.querySelector(".contact form");
+    
+    if (contactForm) {
+        contactForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            alert("El teu missatge ha estat enviat! (Demostració)");
+        });
+    }
+});
