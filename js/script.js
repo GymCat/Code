b0f4069c046957ffe9bc9e57cb2fa61e68a7a712
IsMeduza <maxsanchezmasso@gmail.com>
2023-05-07 16:35:49 +0200

    V1.2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("La web s'ha carregat correctament.");
-    
-    const images = document.querySelectorAll("img");
-
-    for (let i = 0; i < images.length; i++) {
-        images[i].addEventListener("load", function() {
-            console.log(`La imatge ${this.src} s'ha carregat correctament.`);
-        });
-
-        images[i].addEventListener("error", function() {
-            console.error(`La imatge ${this.src} no s'ha pogut carregar.`);
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    // Mostrem el missatge de benvinguda
+    let boto = document.createElement("button");
+    boto.innerText = "Clica aquí";
+    boto.addEventListener("click", function () {
+      alert("Benvingut a la nostra pàgina web GymCat!");
+    });
+  
+    let container = document.getElementById("boto-container");
+    if (container) {
+      container.appendChild(boto);
     }
-
-    const contactForm = document.querySelector(".contact form");
-
-    if (contactForm) {
-        contactForm.addEventListener("submit", function(event) {
-            event.preventDefault();
-            alert("El teu missatge ha estat enviat! (Demostració)");
-        });
+  
+    // Carreguem les classes del fitxer XML
+    if (document.querySelector(".info-container")) {
+      fetch("XML/classes.xml")
+        .then((response) => response.text())
+        .then((xmlText) => {
+          let parser = new DOMParser();
+          let xmlDoc = parser.parseFromString(xmlText, "text/xml");
+          let classes = xmlDoc.querySelectorAll("class");
+  
+          let classesContainer = document.querySelector(".info-container");
+          classesContainer.innerHTML = "";
+  
+          classes.forEach((classElement) => {
+            let classDiv = document.createElement("div");
+            classDiv.classList.add("class");
+  
+            let name = classElement.querySelector("name").textContent;
+            let description = classElement.querySelector("description").textContent;
+            let image = classElement.querySelector("image").textContent;
+  
+            classDiv.innerHTML = `
+              <h2>${name}</h2>
+              <img src="${image}" alt="Classe de ${name}">
+              <p>${description}</p>
+            `;
+  
+            classesContainer.appendChild(classDiv);
+          });
+        })
+        .catch((error) => console.error("Error carregant les dades de les classes:", error));
     }
-});
+  });
